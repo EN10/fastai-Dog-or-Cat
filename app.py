05ce80aeafd2558d47e74c4afe2b7a238ab20878
Jeremy Howard <j@fast.ai>
2022-04-28 08:57:33 +1000

    192

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def classify_image(img):
     pred,idx,probs = learn.predict(img)
     return dict(zip(categories, map(float,probs)))
 
-image = gr.inputs.Image(shape=(128, 128))
+image = gr.inputs.Image(shape=(192, 192))
 label = gr.outputs.Label()
 examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
 
