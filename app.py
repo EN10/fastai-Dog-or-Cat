6855de559203675e3afefa510a080bc44d039d42
Jeremy Howard <j@fast.ai>
2022-04-28 08:49:51 +1000

    track

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+l2 = load_learner('model.lrn')
+categories = ('Dog', 'Cat')
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+def classify_image(img):
+    pred,idx,probs = l2.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+image = gr.inputs.Image(shape=(128, 128))
+label = gr.outputs.Label()
+examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch()
+
